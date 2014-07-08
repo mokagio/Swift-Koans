45243816e4ebeda5dc00670e6936bb9335b0fc6b
Gio <giovanni.lodi42@gmail.com>
2014-07-08 20:46:55 +0200

    Added Quick for RSpec style tests
    See [Quick](https://github.com/Quick/Quick)

---
+++ Swift-KoansTests/SwiftKonasSpec.swift
@@ -0,0 +1,10 @@
+import Quick
+import Nimble
+
+class SwiftKoansSpec: QuickSpec {
+    override func spec() {
+        it("should fails") {
+            expect(false).to.beTrue()
+        }
+    }
+}
