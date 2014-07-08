25de4a50137287b967352bd754fa916b438cb17b
Gio <giovanni.lodi42@gmail.com>
2014-07-08 20:35:20 +0200

    Swift-Koans first commit, Xcode (Beta 2) generated project

---
+++ Swift-Koans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Koans
+//
+//  Created by Gio on 08/07/2014.
+//  Copyright (c) 2014 mokagio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
