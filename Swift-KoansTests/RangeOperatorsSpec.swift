20832bbd4a79f151773b3ee162fe53e95dd6bb2e
Gio <giovanni.lodi42@gmail.com>
2014-07-10 21:52:00 +0200

    Added range operators spec

---
+++ Swift-KoansTests/RangeOperatorsSpec.swift
@@ -0,0 +1,26 @@
+import Quick
+import Nimble
+
+class RangeOperatorsSpec : QuickSpec {
+    override func spec() {
+        describe("Swift has two range operators") {
+            describe("the closed range operator, defines a range that runs from a to b, and includes the values a and b") {
+                let end = 10
+                var counter = 0
+                for i in 1...end {
+                    ++counter
+                }
+                expect(counter).to.equal(end)
+            }
+
+            describe("the half-closed range operator, defines a range that runs from a to b, but does not include b") {
+                let end = 10
+                var counter = 0
+                for i in 1..<end {
+                    ++counter
+                }
+                expect(counter).to.equal(end - 1)
+            }
+        }
+    }
+}
